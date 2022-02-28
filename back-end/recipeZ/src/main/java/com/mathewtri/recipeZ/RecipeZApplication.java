ca57a3c963be4409d7be1066981c49c6ff3b7963
Tri Do <tri.do@sjsu.edu>
2022-02-28 09:26:32 -0800

    generated spring boot project for backend

---
+++ back-end/recipeZ/src/main/java/com/mathewtri/recipeZ/RecipeZApplication.java
@@ -0,0 +1,13 @@
+package com.mathewtri.recipeZ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeZApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeZApplication.class, args);
+	}
+
+}
---
+++ back-end/recipeZ/src/test/java/com/mathewtri/recipeZ/RecipeZApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mathewtri.recipeZ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeZApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
