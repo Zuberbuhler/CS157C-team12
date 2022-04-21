ae8a7d6b1a5447578b245d7c8e8a183182d0410b
Tri Do <tri.do@sjsu.edu>
2022-04-21 15:19:46 -0700

    added authentication, modified the react-router-route

---
+++ front-end/my-recipe/src/components/CreateRecipe.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+export default function CreateRecipe() {
+   return (
+      <>
+         <h1>Create Recipe</h1>
+      </>
+   );
+}
---
+++ front-end/my-recipe/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+
+export default function Navbar() {
+   return (
+      <nav>
+         <ul>
+            <li>Ingredient</li>
+            <li>Recipe</li>
+         </ul>
+      </nav>
+   );
+}
